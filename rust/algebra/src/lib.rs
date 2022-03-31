041f0c938de893e6a83311e65d46c4698cfafcb2
yexincheng <50694493+yexincheng@users.noreply.github.com>
2022-03-31 15:11:30 +1000

    remove the second one as well

--- rust/algebra/src/lib.rs
+++ rust/algebra/src/lib.rs
@@ -4,7 +4,7 @@
     trivial_casts,
     trivial_numeric_casts
 )]
-#![deny(unused_qualifications, variant_size_differences, stable_features)]
+#![deny(unused_qualifications, variant_size_differences)]
 #![deny(
     non_shorthand_field_patterns,
     unused_attributes,
